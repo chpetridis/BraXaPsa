484004a6754211cefb2b93245021095a34b5e5c6
Christos Petridis <32238022+chpetridis@users.noreply.github.com>
2018-01-15 21:11:24 +0200

    Delete Globals.py

--- BraXaPsa-code-only/BXP/Globals.py
+++
@@ -1,8 +0,0 @@
-"""  Used for Globals that communicate with a lot of files """
-
-
-class Globals:
-    active_sounds = True     # Checking if sounds are on/off
-    sounds = []              # List of all the sounds in game
-    hold = True              # for synchronization between Item objects (used in both swap() and play())
-    found_triad = False      # for checking if no one found triad in play()
