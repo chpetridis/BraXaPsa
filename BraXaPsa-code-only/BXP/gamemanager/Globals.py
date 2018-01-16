74ba286f7d99be8c8d310478e3c606c498648c11
Christos Petridis <32238022+chpetridis@users.noreply.github.com>
2018-01-16 20:58:32 +0200

    Update Globals.py

--- BraXaPsa-code-only/BXP/gamemanager/Globals.py
+++ BraXaPsa-code-only/BXP/gamemanager/Globals.py
@@ -2,7 +2,7 @@
 
 
 class Globals:
-    active_sounds = True     # Checking if sounds are on/off
-    sounds = []              # List of all the sounds in game
+    active_sounds = True
+    sounds = []
     hold = True              # for synchronization between Item objects (used in both swap() and play())
-    found_triad = False      # for checking if no one found triad in play()
+    found_triad = False
